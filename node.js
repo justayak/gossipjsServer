cf79d1bcf76ac44c7082962bc269df97509af5b1
Julian <jutanke@googlemail.com>
2014-11-11 00:35:55 +0100

    to server

--- node.js
+++ node.js
@@ -2,6 +2,6 @@
  * Created by julian on 17/10/14.
  */
 var cors = "http://85.25.215.113";
-//cors = "http://localhost:8000";
+cors = "http://localhost:8000";
 var GossipBroker = require("./gossipjs").Broker({port:9000, debug:true, bootstrapPort:9001,cors:cors});
 //GossipBroker.debug('hallo');
