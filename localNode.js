3906da1025959e350c11caea9192d0f5871be1b5
Julian <jutanke@googlemail.com>
2014-11-12 21:07:18 +0100

    switch between node and local

--- localNode.js
+++ localNode.js
@@ -1,5 +1,5 @@
 /**
- * Created by Julian on 17/10/14.
+ * Created by Julian on 12/11/14.
  */
-var cors = "http://85.25.215.113";
+var cors = "http://localhost:8000";
 var GossipBroker = require("./gossipjs").Broker({port:9000, debug:true, bootstrapPort:9001,cors:cors});
